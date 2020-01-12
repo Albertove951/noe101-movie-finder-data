db4829403e97bf12284f0a7db4b715153ff946b1
Alberto Vega <albertovega00@gmail.com>
2020-01-12 10:17:11 -0800

    first commit

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
-
+const morgan = require('morgan');
+const axios = require('axios');
+const key = require('./env')
 
 const app = express();
 
+app.use(morgan('dev'));
 // When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
+
+app.get('/?i=tt3896198', function (req, res) {
+    return axios.get('')
+})
 
 module.exports = app;
