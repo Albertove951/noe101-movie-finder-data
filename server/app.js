0557cae5e2184565416f17f22a870bd80f96b164
Alberto Vega <albertovega00@gmail.com>
2020-01-13 10:01:06 -0800

    added the axios get call

--- server/app.js
+++ server/app.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const morgan = require('morgan');
 const axios = require('axios');
-const key = require('./env')
 
 const app = express();
 
+const cache = {};
+
 app.use(morgan('dev'));
 // When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
-
-app.get('/?i=tt3896198', function (req, res) {
-    return axios.get('')
+app.get(`/`, function (req, res) {
+    return axios.get('http://www.omdbapi.com/?apikey=773f1445&/?i=tt3896198')
+        .then(function (res) {
+            console.log(res)
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+        .finally(function () {
+        })
 })
 
 module.exports = app;
